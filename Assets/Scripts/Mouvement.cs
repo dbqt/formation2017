ed99134de3494c8ff9d149793345f2961b73af86
David Tran <dt_dbqt@hotmail.com>
2017-09-06 01:58:20 -0400

    init

---
+++ Assets/Scripts/Mouvement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouvement : MonoBehaviour {
+
+    // Vitesse de deplacement, la valeur est donnee dans l'inspecteur
+    public float Vitesse;
+	
+	// Update is called once per frame
+	void Update () {
+        // Lire les entrees
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Creer un vecteur de direction de deplacement
+        Vector3 deplacement = new Vector3(horizontal, 0f, vertical);
+
+        // Appliquer le deplacement sur le transform de cet objet.
+        this.gameObject.transform.Translate(deplacement * Vitesse * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Tir.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tir : MonoBehaviour {
+
+    // Reference vers le prefab de l'objet projectile
+    public GameObject PrefabProjectile;
+
+    // Vitesse de deplacement, la valeur est donnee dans l'inspecteur
+    public float VitesseDeTir;
+
+	// Update is called once per frame
+	void Update () {
+        // Detection d'un click
+		if (Input.GetButtonDown("Fire1"))
+        {
+            // Tracer une ligne (ray) de la camera
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            // Detection du point entre la ligne et une surface
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Calcul de la direction du tir
+                Vector3 directionDeTir = (hit.point - this.transform.position);
+
+                // Instanciation d'une projectile a la position de cet objet + direction du tir pour eviter de mettre la projectile dans le joueur
+                GameObject nouveauProjectile = Instantiate(PrefabProjectile, this.transform.position + directionDeTir.normalized, Quaternion.identity) as GameObject;
+                // Donner une vitesse a la projectile
+                nouveauProjectile.GetComponent<Rigidbody>().velocity = directionDeTir.normalized * VitesseDeTir;
+
+            }
+
+        }
+	}
+}
